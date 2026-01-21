c69d41cefd9beb2111cce9fcb9b42223b544830d
kinggeorges12 <kinggeorges1212@gmail.com>
2026-01-20 21:30:42 -0500

    changed parameter in post to string

--- server/routes/user/index.ts
+++ server/routes/user/index.ts
@@ -371,14 +371,14 @@ router.get<{ id: string }, UserRequestsResponse>(
   }
 );
 
-router.post<{ userId: number }, MediaRequest, MediaRequestBody>(
-  '/:userId/request',
+router.post<{ id: string }, MediaRequest, MediaRequestBody>(
+  '/:id/request',
   isAuthenticated(Permission.ADMIN),
   async (req, res, next) => {
     try {
       // Copied from /:id/requests
       const user = await getRepository(User).findOneOrFail({
-        where: { id: Number(req.params.userId) },
+        where: { id: Number(req.params.id) },
       });
 
       if (!user) {
